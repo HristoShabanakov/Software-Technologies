deb81ac2b140c10a0713d3f644a2dd8bb7ab2799
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-09-28 22:22:56 +0200

    JavaScript Syntax and Basic Web

---
+++ 03.JavaScript Syntax and Basic Web/03.Sum Two Numbers/03. Sum Two Numbers.js
@@ -0,0 +1,10 @@
+function sumOfTwoNumbers(arr) {
+    let numberOne = Number(arr[0]);
+    let numberTwo = Number(arr[1]);
+    console.log(numberOne + numberTwo);
+}
+
+let myArr = ['7','8'];
+
+
+sumOfTwoNumbers(myArr);
