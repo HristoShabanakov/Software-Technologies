c2d8c86b404c0ac8d2436b37922fd74cf6430b60
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-11-20 22:14:34 +0100

    PHP Basic Syntax - Lab

---
+++ 07.PHP Basic Syntax - Lab/07.Celsius - Fahrenheit/07. Celsius - Fahrenheit.php
@@ -0,0 +1,40 @@
+<?php
+//Function which calculate degrees from Celsius to Fahrenheit.
+function celsiusToFahrenheit (float $celsius) : float
+{
+    return $celsius * 1.8 +32 ;
+}
+
+function fahrenheitToCelsius (float $fahrenheit) : float
+{
+    return ($fahrenheit -32) / 1.8;
+}
+
+$msgAfterCelsius = "";
+if(isset($_GET['cel'])){
+    //Floatval is the same as parse.float method in C#
+    $cel = floatval($_GET['cel']);
+    $fah = celsiusToFahrenheit($cel);
+    //Print the result
+    $msgAfterCelsius = "$cel &deg;C = $fah &deg;F";
+}
+
+$msgAfterFahrenheit = "";
+if(isset($_GET['fah'])){
+    $fah = floatval($_GET['fah']);
+    $cel = fahrenheitToCelsius($fah);
+    $msgAfterFahrenheit = "$fah &deg;F = $cel &deg;C";
+}
+?>
+
+<form>
+    Celsius: <input type="text" name="cel"/>
+    <input type="submit" value="Convert">
+    <?= $msgAfterCelsius ?>
+</form>
+<form>
+    Fahrenheit: <input type="text" name="fah"/>
+    <input type="submit" value="Convert">
+    <?= $msgAfterFahrenheit ?>
+</form>
+
