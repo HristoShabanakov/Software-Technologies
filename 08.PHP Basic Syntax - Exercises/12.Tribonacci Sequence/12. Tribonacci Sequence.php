652166352a2047083455caf6ea04f4a8659f1470
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-11-24 07:27:52 +0100

    PHP Basic Syntax - Exercises

---
+++ 08.PHP Basic Syntax - Exercises/12.Tribonacci Sequence/12. Tribonacci Sequence.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>First Steps Into PHP</title>
+</head>
+<body>
+    <form>
+        N: <input type="text" name="num" />
+        <input type="submit" />
+    </form>
+    <?php
+    if(isset($_GET['num'])){
+        $num = intval($_GET['num']);
+        $a = 0;
+        $b = 0;
+        $c = 1;
+        for ($i=0; $i <$num; $i++ ){
+            echo $c . " ";
+            $temp = $a +$b + $c;
+            $a = $b;
+            $b = $c;
+            $c = $temp;
+        }
+    }
+    ?>
+</body>
+</html>
