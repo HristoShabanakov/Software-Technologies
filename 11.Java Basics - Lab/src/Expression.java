4be17e2223535ce62458e0bf12d5c0c167958a7c
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-12-03 10:04:17 +0100

    Java Basics - Lab

---
+++ 11.Java Basics - Lab/src/Expression.java
@@ -0,0 +1,7 @@
+public class Expression {
+    public static void main(String[] args) {
+        double number = ((30 + 21) / 2.0 * (35 - 12 - 1 / 2.0));
+        number = Math.pow(number, 2);
+        System.out.println(number);
+    }
+}
