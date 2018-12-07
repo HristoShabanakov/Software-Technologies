d048355a574a5efaa4b00ff83dd08088e0c8b439
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-12-07 10:21:30 +0100

    Java Basics - Exercises

---
+++ 12.Java Basics - Exercises/ReverseCharacters.java
@@ -0,0 +1,15 @@
+package Exercises;
+
+import java.util.Scanner;
+
+public class ReverseCharacters {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char firstSymbol = scanner.nextLine().charAt(0);
+        char secondSymbol = scanner.nextLine().charAt(0);
+        char thirdSymbol = scanner.nextLine().charAt(0);
+        String input = "" + firstSymbol + secondSymbol + thirdSymbol;
+        String result = new StringBuilder(input).reverse().toString();
+        System.out.println(result);
+    }
+}
