c019f3c117c76e150eb1a9a8a6299785e1d3bcc5
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-12-17 15:36:37 +0100

    C#-ASP.NET-MVC-Overview-Lab

---
+++ 15.CSharp-ASP.NET-MVC-Overview-Lab/ToDoList/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            using (var db = new TaskDbContext())
+            {
+                var tasks = db.Tasks.ToList();
+                return View(tasks);
+            }
+        }
+
+        
+    }
+}
