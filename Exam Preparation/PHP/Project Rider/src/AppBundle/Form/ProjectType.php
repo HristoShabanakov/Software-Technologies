e707d6b948f86fdeceb034819e7b6737e6252266
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-12-24 14:31:52 +0100

    PHP - CRUD Operations

---
+++ Exam Preparation/PHP/Project Rider/src/AppBundle/Form/ProjectType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace AppBundle\Form;
+
+//use Doctrine\DBAL\Types\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class ProjectType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title',TextType::class)
+            ->add('description',TextType::class)
+            ->add('budget',NumberType::class);
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Project'
+        ));
+    }
+
+
+
+}
