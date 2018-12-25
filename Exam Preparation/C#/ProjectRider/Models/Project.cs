8d5a237a62388f2ccfbfe2d7d1f93a67a74167f5
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2018-12-25 15:29:05 +0100

    C# - CRUD Operations

---
+++ Exam Preparation/C#/ProjectRider/Models/Project.cs
@@ -0,0 +1,20 @@
+﻿
+namespace ProjectRider.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Project
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        [Range(int.MinValue, int.MaxValue)]
+        public int Budget { get; set; }
+    }
+}
