5677707062c04f9efe45514614d568f49c75dcfa
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2019-01-15 23:43:25 +0100

    Java - CRUD Operations

---
+++ Software Techonolgies Exam - 10th of January 2019/Java/src/main/java/softunilabs/bindingModels/LabBindingModel.java
@@ -0,0 +1,36 @@
+package softunilabs.bindingModels;
+
+
+import javax.validation.constraints.NotNull;
+
+public class LabBindingModel {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(Integer capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    private Integer capacity;
+
+    private String status;
+}
