dd25cf6658a6974193667c10a4f67413592ab9d1
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2019-01-15 23:39:11 +0100

    CRUD Operations - C#

---
+++ Software Techonolgies Exam - 10th of January 2019/C#/Controllers/LabController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using SoftUniLabs.Data;
+using SoftUniLabs.Models;
+
+namespace SoftUniLabs.Controllers
+{
+    public class LabController : Controller
+    {
+        private readonly LabDbContext context;
+
+        public LabController(LabDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        [Route("/")]
+        public IActionResult Index()
+        {
+            List<Lab> labs = this.context.Labs.ToList();
+            return View(labs);
+
+        }
+
+        [HttpGet]
+        [Route("/create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/create")]
+        public IActionResult Create(Lab lab)
+        {
+            this.context.Labs.Add(lab);
+            this.context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        [Route("/edit/{id}")]
+        public IActionResult Edit(int id)
+        {
+            Lab lab = this.context.Labs.Find(id);
+            return View(lab);
+        }
+
+        [HttpPost]
+        [Route("/edit/{id}")]
+        public IActionResult Edit(Lab lab)
+        {
+            this.context.Labs.Update(lab);
+            this.context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        [Route("/delete/{id}")]
+        public IActionResult Delete(int id)
+        {
+            Lab lab = context
+            .Labs
+            .FirstOrDefault(p => p.Id == id);
+            return View(lab);
+
+        }
+
+        [HttpPost]
+        [Route("/delete/{id}")]
+        public IActionResult Delete(Lab lab)
+        {
+            this.context.Labs.Remove(lab);
+            this.context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
